f46048e38c801c9d2f860751ad548f570204de1b
patelankeet2 <patelankeet2@gmail.com>
2025-09-15 19:39:29 +1200

    Added code for contant.js

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+export const API_BASE_URL = 'http://localhost:8080/api';
+export const QUIZ_CATEGORIES = [
+  'General Knowledge', 'Entertainment: Books', 'Entertainment: Film', 
+  'Entertainment: Music', 'Entertainment: Musicals & Theatres', 
+  'Entertainment: Television', 'Entertainment: Video Games', 
+  'Entertainment: Board Games', 'Science & Nature', 'Science: Computers', 
+  'Science: Mathematics', 'Mythology', 'Sports', 'Geography', 'History', 
+  'Politics', 'Art', 'Celebrities', 'Animals', 'Vehicles', 
+  'Entertainment: Comics', 'Science: Gadgets', 
+  'Entertainment: Japanese Anime & Manga', 'Entertainment: Cartoon & Animations'
+];
+export const QUIZ_DIFFICULTIES = ['Easy', 'Medium', 'Hard'];
